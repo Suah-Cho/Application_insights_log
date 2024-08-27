0c3ee68088fb186d03cc6341fa8ccd6aa2a7abed
Suah-Cho <sacho0929@gmail.com>
2024-08-27 22:01:02 +0900

    Update main.py using middleware

--- main.py
+++ main.py
@@ -1,42 +1,58 @@
-from fastapi import FastAPI
-from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-import logging
+from fastapi import FastAPI, Request, Response
+from azure.monitor.opentelemetry import configure_azure_monitor
+from opentelemetry import trace
+from opentelemetry.trace import (
+    SpanKind,
+    get_tracer_provider,
+    set_tracer_provider,
+)
+from opentelemetry.propagate import extract
+from logging import getLogger, INFO
 import os
 
 app = FastAPI()
 
-connection_string = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
-
-
-resource = Resource.create({"service.name": "croft-test"})
-
-provider = TracerProvider(resource=resource)
-span_processor = BatchSpanProcessor(
-    AzureMonitorTraceExporter.from_connection_string(connection_string)
+configure_azure_monitor(
+    connection_string=os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
 )
-provider.add_span_processor(span_processor)
 
-from opentelemetry import trace
-trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__, 
+                          tracer_provider=trace.get_tracer_provider()
+)
 
-FastAPIInstrumentor.instrument_app(app)
+logger = getLogger(__name__)
+logger.setLevel(INFO)
+
+@app.middleware("http")
+async def add_trace_data(request: Request, call_next):
+    # Trace Context 추출
+    with tracer.start_as_current_span(
+        "main-20240827",
+        context=extract(request.headers),
+        kind=SpanKind.SERVER
+    ) as span:
+        # 요청 처리 전 로그 기록
+        # logger.info("INFO log - Before Request.")
+        
+        # 요청 처리
+        response = await call_next(request)
+
+        # 요청 처리 후 로그 및 커스텀 속성 추가
+        span.set_attribute('http.url', str(request.url))
+        span.set_attribute('http.method', str(request.method))
+        span.set_attribute('http.status_code', response.status_code)
+
+        # logger.info("INFO log - After Request.")
+
+        return response
 
 @app.get('/')
 def root():
-    logging.info("test1 - info")
-    logging.warning("test2 - warning")
-    logging.error("test3 - error")
-    return {"message": "Hello World"}
+    return {"message": "Hello, World!"}
 
-@app.get('/application')
-def application():
-    return {"message": "Hello World from application"}
 
-@app.get('/insights')
-def insights():
-    return {"message": "Hello World from insights"}
+
+@app.get('/test')
+def test(request: Request):
+    logger.info("INFO log - Test!!!!!!!!!!!!")
+    return {"message": "Hello, Test!"}
